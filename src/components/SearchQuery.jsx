41b4204e98fbfd04f9fa53323be0c2e00fc2def2
Karan Dhillon <Karanpal.dhillon@gmail.com>
2024-01-22 09:03:04 +0530

    use ref

--- src/components/SearchQuery.jsx
+++ src/components/SearchQuery.jsx
@@ -1,5 +1,25 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
+import { useRef } from "react";
 export const SearchQuery = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+  useEffect(
+    function() {
+      function callback(e) {
+        if (document.activeElement === inputEl) return;
+        if (e.code.toLowerCase() === "enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return function() {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [setQuery],
+  );
+
   return (
     <input
       className="search"
@@ -7,6 +27,7 @@ export const SearchQuery = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
